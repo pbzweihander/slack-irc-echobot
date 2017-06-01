0cd49e9ca128a023160d4e45aed4ed78cfab7b7b
pbzweihander <sd852456@naver.com>
2017-06-01 13:43:26 +0900

    thread를 두개로 줄이고 setechohere 없이 setting.py에 의존, 파싱 코드 개선 등

--- main.py
+++ main.py
@@ -2,22 +2,14 @@
 from irc import IRC
 from slack import Slack
 from threading import Thread
-from queue import Queue
 from settings import *
 
 
 irc = IRC()
 slack = Slack(SLACK_TOKEN, BOT_NAME)
-irc_queue = Queue()
-slack_queue = Queue()
-
-irc_channel_to_echo = ""
-irc_nicknames_to_ignore = []
-slack_channel_to_echo = ""
-slack_nicknames_to_ignore = []
 
 
-class IRCReadingThread(Thread):
+class IRCThread(Thread):
     __suspend = False
 
     def __init__(self):
@@ -34,22 +26,22 @@ class IRCReadingThread(Thread):
             for text in lines:
                 if not text:
                     continue
-
-                if 'PING ' in text:
-                    irc.raw_send('PONG ' + text.split()[1])
-                elif 'INVITE ' in text:  # 유저가 채널로 초대시 응답
-                    irc.join(text.split(':', 2)[-1])
-                elif 'PRIVMSG ' in text:
-                    chan = text.split("PRIVMSG ")[1].split()[0]
-                    sender = text.split("!")[0][1:]
-                    msg = text.split(":", 2)[2]
-
-                    if "#" in chan:
-                        irc_queue.put((chan, sender, msg))
+                if text.startswith("PING"):
+                    irc.raw_send('PONG ' + text[5:])
+                else:
+                    prefix = text.find('PRIVMSG #%s :' % IRC_CHANNEL)
+                    len_prefix = len('PRIVMSG #%s :' % IRC_CHANNEL)
+                    if prefix != -1:
+                        msg = text[prefix + len_prefix:]
+                        for uid in slack.users:
+                            msg = msg.replace('<@' + uid + '>', '<@' + slack.users[uid] + '>')
+                        username = text[1:text.find('!')]
+                        slack.post_message('#' + SLACK_CHANNEL, msg, False, username)
+                        print("irc -> slack - " + username + ": " + msg)
         print("IRC Reading Thread Suspended")
 
 
-class SlackReadingThread(Thread):
+class SlackThread(Thread):
     __suspend = False
 
     def __init__(self):
@@ -62,115 +54,22 @@ class SlackReadingThread(Thread):
     def run(self):
         print("Slack Reading Thread run")
         while not self.__suspend:
-            d = slack.read()
-            if d and d.get('subtype') != "bot_message" and d.get('user') != slack.id:
-                userid = d.get('user')
-                username = slack.users.get(userid)
-                if not username:
-                    slack.refresh_users()
-                    username = slack.users.get(userid) or "?"
-                chan = d.get('channel')
-                msg = d.get('text')
-                for id in slack.users:
-                    msg = str.replace(msg, '<@' + id + '>', '<@' + slack.users[id] + '>')
-                msg = str.replace(msg, '&lt;', '<').replace('&gt;', '>')
-                slack_queue.put((chan, username, msg))
+            texts = slack.read()
+            for d in texts:
+                if d.get('type') == 'message' and 'subtype' not in d:
+                    if slack.channels.get(d.get('channel')) == SLACK_CHANNEL:
+                        userid = d.get('user')
+                        username = slack.users.get(userid)
+                        if not username:
+                            slack.refresh_users()
+                            username = slack.users.get(userid) or "?"
+                        msg = d.get('text')
+                        for uid in slack.users:
+                            msg = str.replace(msg, '<@' + uid + '>', '<@' + slack.users[uid] + '>')
+                        msg = str.replace(msg, '&lt;', '<').replace('&gt;', '>')
+                        irc.send(IRC_CHANNEL, username + ": " + msg)
+                        print("slack -> irc - " + username + ": " + msg)
         print("Slack Reading Thread Suspended")
-
-
-class IRCHandlingThread(Thread):
-    __suspend = False
-
-    def __init__(self):
-        Thread.__init__(self)
-        print("IRC Handling Thread __init__")
-
-    def suspend(self):
-        self.__suspend = True
-
-    def run(self):
-        global irc_channel_to_echo, irc_nicknames_to_ignore
-        print("IRC Handling Thread run")
-        while not self.__suspend:
-            if not irc_queue.empty():
-                t = irc_queue.get()
-                chan = t[0]
-                name = t[1]
-                msg = t[2]
-
-                if str.startswith(msg, '?setechohere'):
-                    irc_channel_to_echo = chan
-                    irc.send(chan, "! Echo here")
-                    print("IRC Channel Set : " + chan)
-                else:
-                    if irc_channel_to_echo and chan == irc_channel_to_echo:
-                        if str.startswith(msg, '?setignore'):
-                            for n in str.split(msg, '?setignore')[1].strip().split():
-                                irc_nicknames_to_ignore.append(n)
-                                irc.send(chan, "! Ignore " + n)
-                                print("IRC Ignore Set : " + n)
-                        elif str.startswith(msg, '?unsetignore'):
-                            for n in str.split(msg, '?unsetignore')[1].strip().split():
-                                if n in irc_channel_to_echo:
-                                    irc_nicknames_to_ignore.remove(n)
-                                    irc.send(chan, "! Do not ignore " + n)
-                                    print("IRC Ignore Unset : " + n)
-                        else:
-                            if name not in irc_nicknames_to_ignore:
-                                if slack_channel_to_echo:
-                                    # outname = chr(8203).join(name)
-                                    # slack.post_message(slack_channel_to_echo, outname + ": " + msg)
-                                    # slack.post_message(slack_channel_to_echo, name + ": " + msg)
-                                    slack.post_message(slack_channel_to_echo, msg, False, name)
-                                    print("irc -> slack - " + name + ": " + msg)
-        print("IRC Handling Thread Suspended")
-
-
-class SlackHandlingThread(Thread):
-    __suspend = False
-
-    def __init__(self):
-        Thread.__init__(self)
-        print("Slack Handling Thread __init__")
-
-    def suspend(self):
-        self.__suspend = True
-
-    def run(self):
-        global slack_channel_to_echo, slack_nicknames_to_ignore
-        print("Slack Handling Thread run")
-        while not self.__suspend:
-            if not slack_queue.empty():
-                t = slack_queue.get()
-                chan = t[0]
-                name = t[1]
-                msg = t[2]
-
-                if str.startswith(msg, '?setechohere'):
-                    slack_channel_to_echo = chan
-                    slack.post_message(chan, "! Echo here")
-                    print("Slack Channel Set : " + chan)
-                else:
-                    if slack_channel_to_echo and chan == slack_channel_to_echo:
-                        if str.startswith(msg, '?setignore'):
-                            for n in str.split(msg, '?setignore')[1].strip().split():
-                                slack_nicknames_to_ignore.append(n)
-                                slack.post_message(chan, "! Ignore " + n)
-                                print("Slack Ignore Set : " + n)
-                        elif str.startswith(msg, '?unsetignore'):
-                            for n in str.split(msg, '?unsetignore')[1].strip().split():
-                                if n in slack_nicknames_to_ignore:
-                                    slack_nicknames_to_ignore.remove(n)
-                                    slack.post_message(chan, "! Do not ignore " + n)
-                                    print("Slack Ignore Unset : " + n)
-                        else:
-                            if name not in slack_nicknames_to_ignore:
-                                if irc_channel_to_echo:
-                                    # outname = chr(8203).join(name)
-                                    # irc.send(irc_channel_to_echo, outname + ": " + msg)
-                                    irc.send(irc_channel_to_echo, name + ": " + msg)
-                                    print("slack -> irc - " + name + ": " + msg)
-        print("Slack Handling Thread Suspended")
 
 
 def main():
@@ -180,17 +79,13 @@ def main():
 
     print("Clients Loaded")
 
-    irc_reading_thread = IRCReadingThread()
-    irc_handling_thread = IRCHandlingThread()
-    slack_reading_thread = SlackReadingThread()
-    slack_handling_thread = SlackHandlingThread()
+    irc_thread = IRCThread()
+    slack_thread = SlackThread()
 
     print("Thread Initialized")
 
-    irc_reading_thread.start()
-    slack_reading_thread.start()
-    irc_handling_thread.start()
-    slack_handling_thread.start()
+    irc_thread.start()
+    slack_thread.start()
 
     while True:
         try:
@@ -200,10 +95,8 @@ def main():
 
     print("Suspending All Threads...")
 
-    irc_reading_thread.suspend()
-    irc_handling_thread.suspend()
-    slack_reading_thread.suspend()
-    slack_handling_thread.suspend()
+    irc_thread.suspend()
+    slack_thread.suspend()
 
 
 if __name__ == '__main__':
